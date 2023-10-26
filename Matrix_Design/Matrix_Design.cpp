4ff741afc56613d48bc0852618dfea4c31ad4691
lilei <37171564+xiaoleitongxue@users.noreply.github.com>
2023-10-26 18:46:48 +0800

    Add project files.

---
+++ Matrix_Design/Matrix_Design.cpp
@@ -0,0 +1,12 @@
+﻿// Matrix_Design.cpp : Defines the entry point for the application.
+//
+
+#include "Matrix_Design.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Matrix_Design/Matrix_Design.h
@@ -0,0 +1,221 @@
+﻿// Matrix_Design.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+template<typename T, size_t N>
+struct Matrix_init {
+	using type = std::initializer_list<typename Matrix_init<T, N - 1>::type>;
+};
+
+template<typename T>
+struct Matrix_init<T, 1> {
+	using type = std::initializer_list<T>;
+};
+
+template<typename T, size_t N>
+using Matrix_initializer = typename Matrix_init<T, N>::type;
+
+
+
+// TODO: Reference additional headers your program requires here.
+#include <vector>
+#include <numeric>
+template<size_t N>
+struct Matrix_slice {
+	Matrix_slice() = default; // empty matrix
+	Matrix_slice(size_t s, std::initializer_list<size_t> exts);
+	Matrix_slice(size_t s, std::initializer_list<size_t> exts, std::initializer_list<size_t> strs);
+	template<typename... Dims>
+	Matrix_slice(Dims... dims);
+	template<typename... Dims, typename = std::enable_if<All(Convertible<Dims, size_t>()...)>>
+	size_t operator()(Dims... dims) const;
+	size_t size; // total number of elements
+	size_t start; // starting offset
+	std::array<size_t, N> extents; // number of elemens in each dimension
+	std::array<size_t, N> strides; // offsets between elements in each dimension
+};
+
+template<typename T, size_t N>
+class Matrix_base {
+	// common stuff
+public:
+	static constexpr size_t order = N; // dim
+	using value_type = T;
+	using iterator = typename std::vector<T>::iterator;
+	using const_iterator = typename std::vector<T>::const_iterator;
+
+	
+
+		
+};
+
+
+template<typename T, size_t N>
+class Matrix_ref : public Matrix_base<T, N> {
+	// special to matrix_ref
+public:
+	Matrix_ref(const Matrix_slice<N>& s, T* p):desc{s}, ptr{p}{}
+private:
+	Matrix_slice<N> desc; // the shape of matrix
+	T* ptr; // the first element in the matrix
+};
+
+template<typename T, size_t N>
+class Matrix : public Matrix_base<T, N> {
+	// special to matrix
+public:
+	Matrix() = default;
+	Matrix(Matrix&) = default; // move constructor
+	Matrix& operator=(Matrix&&) = default; // move assign
+	Matrix(Matrix const&) = default;
+	Matrix& operator=(Matrix const&) = default;
+	~Matrix() = default;
+
+	static constexpr size_t order() { return N; } // number of dimensions
+	size_t extent(size_t n) const { return desc.extents[n]; } // # elements in the nth dimension
+
+	template<typename U>
+	Matrix(const Matrix_ref<U, N>&); // construct from Matrix_ref
+	
+	template<typename U>
+	Matrix& operator=(const Matrix_ref<U, N>&); //assign from Matrix_ref
+
+	template<typename... Exts> // specify the extents
+	explicit Matrix(Exts... exts); // init from dims
+
+	Matrix(Matrix_initializer<T, N>); // initializer from list
+	Matrix& operator=(Matrix_initializer<T, N>); // assign from list
+
+	template<typename U>
+	Matrix(std::initializer_list<U>) = delete; // don't use {} except for elements
+
+	template<typename U>
+	Matrix& operator=(std::initializer_list<U>) = delete;
+
+	size_t size() const { return elements.size(); } // total number of elements
+	
+	const Matrix_slice<N>& descriptor() const { return desc; } // the slice defining subscripting
+
+	T* data() { return elements.data(); } // "flat" element access
+	
+	const T* data() const { return elements.data(); }
+
+	//template<typename F>
+	//Matrix& apply(F f);
+
+	//template<typename M, typename F>
+	//Matrix& apply(const M& m, F f);
+
+	//Matrix& operator=(const T& value);
+	//Matrix& operator+=(const T& value);
+	//Matrix& operator-+(const T & value);
+	//Matrix& operator*+(const T & value);
+	//Matrix& operator/+(const T & value);
+
+private:
+	Matrix_slice<N> desc;
+	std::vector<T> elements;
+};
+
+template<size_t N>
+template<typename ...Dims>
+inline Matrix_slice<N>::Matrix_slice(Dims... dims):extents{size_t(dims)...}
+{
+	static_assert(sizeof...(Dims) == N, "");
+	size = (dims * ...);
+	size_t args[N]{ size_t(dims)... }; // copy arguments into an array
+	size_t temp[N + 2]{ 1 };
+	for (int i = 0; i < N; ++i) {
+		temp[i] = args[i];
+	}
+	for (int i = N + 2 - 2; i >= 0; --i) {
+		temp[i] = temp[i] * temp[i + 1];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		strides[i] = temp[i];
+	}
+	start = size_t(0);
+
+}
+
+template<size_t N>
+template<typename ...Dims, typename>
+inline size_t Matrix_slice<N>::operator()(Dims ...dims) const
+{
+	static_assert(sizeof...(Dims) == N, "");
+	size_t args[N]{ size_t(dims)... }; // copy arguments into an array
+	return std::inner_product(args, args + N, strides.begin(), size_t(0));
+}
+
+template<size_t N>
+inline Matrix_slice<N>::Matrix_slice(size_t s, std::initializer_list<size_t> exts):start{s}
+{
+	static_assert(exts.size() == N, "");
+	// init size
+	int product = 1;  // 初始化累积乘积为1
+
+	for (int num : exts) {
+		product *= num;
+	}
+	size = product;
+
+
+	// init extents
+	for (size_t i = 0; i < exts.size(); ++i) {
+		extents[i] = exts[i];
+	}
+
+	// init strides
+	size_t temp[N + 2]{ 1 };
+	for (int i = 0; i < N; ++i) {
+		temp[i] = exts[i];
+	}
+	for (int i = N + 2 - 2; i >= 0; --i) {
+		temp[i] = temp[i] * temp[i + 1];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		strides[i] = temp[i];
+	}
+}
+
+template<size_t N>
+inline Matrix_slice<N>::Matrix_slice(size_t s, std::initializer_list<size_t> exts, std::initializer_list<size_t> strs) :start{s}
+{
+	static_assert(exts.size() == N, "");
+	int product = 1;  // 初始化累积乘积为1
+
+	for (int num : exts) {
+		product *= num;
+	}
+	size = product;
+
+	// init extents
+	for (size_t i = 0; i < exts.size(); ++i) {
+		extents[i] = exts[i];
+	}
+
+	// init strides
+	for (size_t i = 0; i < strs.size(); ++i) {
+		strides[i] = strs[i];
+	}
+	
+
+
+
+}
+
+template<typename T, size_t N>
+template<typename ...Exts>
+inline Matrix<T, N>::Matrix(Exts ...exts) :desc{ exts }, elements((exts * ...), 0)
+{
+	// init elements
+
+
+}
+
+
