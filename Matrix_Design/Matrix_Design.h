1764a2b8134e06ffcdd80da773fb21d593940185
lilei <37171564+xiaoleitongxue@users.noreply.github.com>
2023-10-27 20:23:02 +0800

    add test

--- Matrix_Design/Matrix_Design.h
+++ Matrix_Design/Matrix_Design.h
@@ -5,213 +5,209 @@
 
 #include <iostream>
 #include <array>
-namespace Matrix_impl {
-	template<typename T, size_t N>
-	struct Matrix_init {
+namespace Matrix_impl
+{
+	template <typename T, size_t N>
+	struct Matrix_init
+	{
 		using type = std::initializer_list<typename Matrix_init<T, N - 1>::type>;
 	};
 
-	template<typename T>
-	struct Matrix_init<T, 1> {
+	template <typename T>
+	struct Matrix_init<T, 1>
+	{
 		using type = std::initializer_list<T>;
 	};
 }
 
-
-
-
-
-template<typename T, size_t N>
+template <typename T, size_t N>
 using Matrix_initializer = typename Matrix_impl::Matrix_init<T, N>::type;
-
-
 
 // TODO: Reference additional headers your program requires here.
 #include <vector>
 #include <numeric>
-template<size_t N>
-struct Matrix_slice {
+template <size_t N>
+struct Matrix_slice
+{
 	Matrix_slice() = default; // empty matrix
 	Matrix_slice(size_t s, std::initializer_list<size_t> exts);
 	Matrix_slice(size_t s, std::initializer_list<size_t> exts, std::initializer_list<size_t> strs);
-	template<typename... Dims>
+	template <typename... Dims>
 	Matrix_slice(Dims... dims);
-	template<typename... Dims, typename = std::enable_if<All(Convertible<Dims, size_t>()...)>>
+	template <typename... Dims, typename = std::enable_if<All(std::is_convertible<Dims, size_t>()...)>>
 	size_t operator()(Dims... dims) const;
-	size_t size; // total number of elements
-	size_t start; // starting offset
+	size_t size;				   // total number of elements
+	size_t start;				   // starting offset
 	std::array<size_t, N> extents; // number of elemens in each dimension
 	std::array<size_t, N> strides; // offsets between elements in each dimension
 };
 
-template<typename T, size_t N>
-class Matrix_base {
+template <typename T, size_t N>
+class Matrix_base
+{
 	// common stuff
 public:
 	using value_type = T;
 	using iterator = typename std::vector<T>::iterator;
-	using const_iterator = typename std::vector<T>::const_iterator;		
+	using const_iterator = typename std::vector<T>::const_iterator;
 };
 
-
-template<typename T, size_t N>
-class Matrix_ref : public Matrix_base<T, N> {
+template <typename T, size_t N>
+class Matrix_ref : public Matrix_base<T, N>
+{
 	// special to matrix_ref
 public:
-	Matrix_ref(const Matrix_slice<N>& s, T* p):desc{s}, ptr{p}{}
+	Matrix_ref(const Matrix_slice<N> &s, T *p) : desc{s}, ptr{p} {}
+
 private:
 	Matrix_slice<N> desc; // the shape of matrix
-	T* ptr; // the first element in the matrix
+	T *ptr;				  // the first element in the matrix
 };
 
-template<typename T, size_t N>
-class Matrix : public Matrix_base<T, N> {
+template <typename T, size_t N>
+class Matrix : public Matrix_base<T, N>
+{
 	// special to matrix
 public:
 	static constexpr size_t order = N; // dim
 	Matrix() = default;
-	Matrix(Matrix&&) = default; // move constructor
-	Matrix& operator=(Matrix&&) = default; // move assignment
-	Matrix(Matrix const&) = default; // copy constructor
-	Matrix& operator=(Matrix const&) = default; // copy assignment
+	Matrix(Matrix &&) = default;				 // move constructor
+	Matrix &operator=(Matrix &&) = default;		 // move assignment
+	Matrix(Matrix const &) = default;			 // copy constructor
+	Matrix &operator=(Matrix const &) = default; // copy assignment
 	~Matrix() = default;
 
-	//static constexpr size_t order() { return N; } // number of dimensions
+	// static constexpr size_t order() { return N; } // number of dimensions
 	size_t extent(size_t n) const { return desc.extents[n]; } // # elements in the nth dimension
 
-	template<typename U>
-	Matrix(const Matrix_ref<U, N>&); // construct from Matrix_ref
-	
-	template<typename U>
-	Matrix& operator=(const Matrix_ref<U, N>&); //assign from Matrix_ref
+	template <typename U>
+	Matrix(const Matrix_ref<U, N> &); // construct from Matrix_ref
 
-	template<typename... Exts> // specify the extents
+	template <typename U>
+	Matrix &operator=(const Matrix_ref<U, N> &); // assign from Matrix_ref
+
+	template <typename... Exts>	   // specify the extents
 	explicit Matrix(Exts... exts); // init from dims
 
-	Matrix(Matrix_initializer<T, N>); // initializer from list
-	Matrix& operator=(Matrix_initializer<T, N>); // assign from list
+	Matrix(Matrix_initializer<T, N>);			 // initializer from list
+	Matrix &operator=(Matrix_initializer<T, N>); // assign from list
 
-	template<typename U>
+	template <typename U>
 	Matrix(std::initializer_list<U>) = delete; // don't use {} except for elements
 
-	template<typename U>
-	Matrix& operator=(std::initializer_list<U>) = delete;
+	template <typename U>
+	Matrix &operator=(std::initializer_list<U>) = delete;
 
 	size_t size() const { return elements.size(); } // total number of elements
-	
-	const Matrix_slice<N>& descriptor() const { return desc; } // the slice defining subscripting
 
-	T* data() { return elements.data(); } // "flat" element access
-	
-	const T* data() const { return elements.data(); }
+	const Matrix_slice<N> &descriptor() const { return desc; } // the slice defining subscripting
 
-	//template<typename F>
-	//Matrix& apply(F f);
+	T *data() { return elements.data(); } // "flat" element access
 
-	//template<typename M, typename F>
-	//Matrix& apply(const M& m, F f);
+	const T *data() const { return elements.data(); }
 
-	//Matrix& operator=(const T& value);
-	//Matrix& operator+=(const T& value);
-	//Matrix& operator-+(const T & value);
-	//Matrix& operator*+(const T & value);
-	//Matrix& operator/+(const T & value);
+	// template<typename F>
+	// Matrix& apply(F f);
 
-	
+	// template<typename M, typename F>
+	// Matrix& apply(const M& m, F f);
+
+	// Matrix& operator=(const T& value);
+	// Matrix& operator+=(const T& value);
+	// Matrix& operator-+(const T & value);
+	// Matrix& operator*+(const T & value);
+	// Matrix& operator/+(const T & value);
 
 private:
 	Matrix_slice<N> desc;
 	std::vector<T> elements;
 };
 
-template<size_t N>
-template<typename ...Dims>
-inline Matrix_slice<N>::Matrix_slice(Dims... dims):extents{size_t(dims)...}, strides(N)
+template <size_t N>
+template <typename... Dims>
+inline Matrix_slice<N>::Matrix_slice(Dims... dims) : strides{N}
 {
 	static_assert(sizeof...(Dims) == N, "");
-	size = (dims * ...);
-	size_t args[N]{ size_t(dims)... }; // copy arguments into an array
+	extents = {size_t(dims)...};
+	size = (dims * ... * 1);
+	size_t args[N]{size_t(dims)...}; // copy arguments into an array
 	size_t temp[N + 2];
 	std::fill(temp, temp + N + 2, size_t(1));
-	for (int i = 0; i < N; ++i) {
+	for (int i = 0; i < N; ++i)
+	{
 		temp[i] = args[i];
 	}
-	for (int i = N + 2 - 3; i >= 0; --i) {
+	for (int i = N + 2 - 3; i >= 0; --i)
+	{
 		strides[i] = 1;
-		for(int j = N + 2 - 1; j > i; --j){
+		for (int j = N + 2 - 1; j > i; --j)
+		{
 			strides[i] *= temp[j];
 		}
 	}
 	start = size_t(0);
-
 }
 
-template<size_t N>
-template<typename ...Dims, typename>
-inline size_t Matrix_slice<N>::operator()(Dims ...dims) const
+template <size_t N>
+template <typename... Dims, typename>
+inline size_t Matrix_slice<N>::operator()(Dims... dims) const
 {
 	static_assert(sizeof...(Dims) == N, "");
-	size_t args[N]{ size_t(dims)... }; // copy arguments into an array
+	size_t args[N]{size_t(dims)...}; // copy arguments into an array
 	return std::inner_product(args, args + N, strides.begin(), size_t(0));
 }
 
-template<size_t N>
-inline Matrix_slice<N>::Matrix_slice(size_t s, std::initializer_list<size_t> exts):start{s}
+template <size_t N>
+inline Matrix_slice<N>::Matrix_slice(size_t s, std::initializer_list<size_t> exts) : extents{exts},start{s}
 {
 	static_assert(exts.size() == N, "");
 	// init size
-	int product = 1;  // 初始化累积乘积为1
+	int product = 1; // 初始化累积乘积为1
 
-	for (int num : exts) {
+	for (int num : exts)
+	{
 		product *= num;
 	}
 	size = product;
 
-
 	// init extents
-	for (size_t i = 0; i < exts.size(); ++i) {
-		extents[i] = exts[i];
-	}
+	// for (size_t i = 0; i < exts.size(); ++i)
+	// {
+	// 	extents[i] = exts[i];
+	// }
 
 	// init strides
-	size_t temp[N + 2]{ 1 };
-	for (int i = 0; i < N; ++i) {
-		temp[i] = exts[i];
+	size_t temp[N + 2]{1};
+	for (int i = 0; i < N; ++i)
+	{
+		temp[i] = extents[i];
 	}
-	for (int i = N + 2 - 2; i >= 0; --i) {
+	for (int i = N + 2 - 2; i >= 0; --i)
+	{
 		temp[i] = temp[i] * temp[i + 1];
 	}
 
-	for (int i = 0; i < N; ++i) {
+	for (int i = 0; i < N; ++i)
+	{
 		strides[i] = temp[i];
 	}
 }
 
-template<size_t N>
-inline Matrix_slice<N>::Matrix_slice(size_t s, std::initializer_list<size_t> exts, std::initializer_list<size_t> strs) :start{s}
+template <size_t N>
+inline Matrix_slice<N>::Matrix_slice(size_t s, std::initializer_list<size_t> exts, std::initializer_list<size_t> strs) : extents{exts},strides{strs},start{s}
 {
 	static_assert(exts.size() == N, "");
-	int product = 1;  // 初始化累积乘积为1
+	int product = 1; // 初始化累积乘积为1
 
-	for (int num : exts) {
+	for (int num : exts)
+	{
 		product *= num;
 	}
 	size = product;
-
-	// init extents
-	for (size_t i = 0; i < exts.size(); ++i) {
-		extents[i] = exts[i];
-	}
-
-	// init strides
-	for (size_t i = 0; i < strs.size(); ++i) {
-		strides[i] = strs[i];
-	}
 }
 
 template <typename T, size_t N>
 template <typename... Exts>
-inline Matrix<T, N>::Matrix(Exts... exts):desc{size_t(exts)...}, elements(desc.size)
+inline Matrix<T, N>::Matrix(Exts... exts) : desc{size_t(exts)...}, elements(desc.size)
 {
-
 }
---
+++ Matrix_Design/Test/Matrix_Design_Test.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include <climits>
+#include <concepts>
+#include <cstdint>
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+#include <vector>
+#include "../Matrix_Design.h"
+#include <algorithm>
+template<typename T, size_t N>
+bool CompareArrays(const std::array<T, N> arr1, const std::array<T, N> arr2, int size) {
+    for (int i = 0; i < size; ++i) {
+        if (arr1[i] != arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int sum(int const& value1, int const& value2)
+{
+    return value1 + value2;
+}
+
+TEST(Matrix_design_test, Simple_test){
+    sum(1,2);
+}
+
+TEST(Matrix_design_test, Matrix_1){
+    Matrix<int, 1> m1(3);
+    EXPECT_EQ(m1.descriptor().size, 3);
+    EXPECT_EQ(m1.descriptor().start, 0);
+    std::array<size_t, 1> expected_extents{3};
+    std::array<size_t, 1> expected_strides{1};
+    EXPECT_TRUE(CompareArrays(m1.descriptor().extents, expected_extents, 1));
+    EXPECT_TRUE(CompareArrays(m1.descriptor().strides, expected_strides, 1));
+    
+}
+
+TEST(Matrix_design_test, Matrix_2){
+    Matrix<int, 2> m2(3, 4);
+    EXPECT_EQ(m2.descriptor().size, 12);
+    EXPECT_EQ(m2.descriptor().start, 0);
+    std::array<size_t, 2> expected_extents{3, 4};
+    std::array<size_t, 2> expected_strides{4, 1};
+    EXPECT_TRUE(CompareArrays(m2.descriptor().extents, expected_extents, 2));
+    EXPECT_TRUE(CompareArrays(m2.descriptor().strides, expected_strides, 2));
+}
+
+TEST(Matrix_design_test, Matrix_3){
+    
+    Matrix<int, 3> m3(3,4,2);
+
+    EXPECT_EQ(m3.descriptor().size, 24);
+    EXPECT_EQ(m3.descriptor().start, 0);
+    std::array<size_t, 3> expected_extents{3, 4, 2};
+    std::array<size_t, 3> expected_strides{8, 2, 1};
+    EXPECT_TRUE(CompareArrays(m3.descriptor().extents, expected_extents, 3));
+    EXPECT_TRUE(CompareArrays(m3.descriptor().strides, expected_strides, 3));
+}
